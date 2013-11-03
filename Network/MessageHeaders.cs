e65273f5a90fe2e36a28feb508b1ac37441240c5
Naureg <joe.lancas@gmail.com>
2013-11-03 14:38:36 +0000

    Changed MessageHeaders to an enum of bytes

--- Network/MessageHeaders.cs
+++ Network/MessageHeaders.cs
@@ -4,13 +4,13 @@
      * in order for the server/client to determine the contents
      * or purpose of the incoming data
      */
-    public struct MessageHeaders{
-        public const byte MESSAGE = 0x0;
-        public const byte SET_ALIAS = 0x1;
-        public const byte REQUEST_PEERS = 0x2;
-        public const byte RECEIVED_PEERS = 0x3;
-        public const byte RECEIVED_PEER = 0x4;
-        public const byte TIMESTAMP = 0x5;
-        public const byte DISCONNECT = 0x6;
+    enum MessageHeaders : byte{
+        MESSAGE = 0x0,
+        SET_ALIAS = 0x1,
+        REQUEST_PEERS = 0x2,
+        RECEIVED_PEERS = 0x3,
+        RECEIVED_PEER = 0x4,
+        TIMESTAMP = 0x5,
+        DISCONNECT = 0x6,
     }
 }
