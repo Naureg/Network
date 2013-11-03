8f2aeeb04d30accd8debc8044ebc302c4228b878
Naureg <joe.lancas@gmail.com>
2013-11-03 11:51:34 +0000

    Initial commit with message headers

---
+++ Network/Network.cs
@@ -0,0 +1,19 @@
+﻿namespace Network{
+    public static class Network{
+
+    }
+    /*
+     * Message headers are prepended to the outgoing bytes
+     * in order for the server/client to determine the contents
+     * or purpose of the incoming data
+     */
+    public struct MessageHeaders{
+        public const byte MESSAGE = 0x0;
+        public const byte SET_ALIAS = 0x1;
+        public const byte REQUEST_PEERS = 0x2;
+        public const byte RECEIVED_PEERS = 0x3;
+        public const byte RECEIVED_PEER = 0x4;
+        public const byte TIMESTAMP = 0x5;
+        public const byte DISCONNECT = 0x6;
+    }
+}
