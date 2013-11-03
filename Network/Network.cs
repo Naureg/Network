0285db12d60a31220b8adf6fab6f34f30ffeb29b
Naureg <joe.lancas@gmail.com>
2013-11-03 14:28:28 +0000

    Implemented abstract Network

---
+++ Network/Network.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Network {
+    abstract class Network{
+        public Dictionary<byte, String> Peers = new Dictionary<byte, String>();
+        public abstract byte[] ReceiveData(Socket socket);
+        public abstract void Disconnect(Socket socket);
+    }
+}
