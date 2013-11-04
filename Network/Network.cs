5947b3e739358a03bda7e4d1746495ddb26726af
Naureg <joe.lancas@gmail.com>
2013-11-04 07:54:45 +0000

    Network no longer abstract

--- Network/Network.cs
+++ Network/Network.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 
 namespace Network {
-    public abstract class Network{
-        public Dictionary<byte, String> Peers = new Dictionary<byte, String>();
-        public abstract byte[] ReceiveData(Socket socket);
-        public abstract void Disconnect(Socket socket);
+    public class Network{
+        public static Dictionary<byte, String> Peers = new Dictionary<byte, String>();
+        public static byte[] ReceiveData(Socket socket){ //receives and trims raw data
+            byte[] buffer = new byte[256];
+            int receivedLength = socket.Receive(buffer);
+            byte[] trimmedBuffer = new byte[receivedLength];
+            Array.Copy(buffer, trimmedBuffer, receivedLength);
+            return trimmedBuffer;
+        }
+        public static void Disconnect(Socket socket, byte ID){
+            
+        }
+        public static void SendData(Socket socket, byte[] data, byte ID){
+            
+        }
+        public static void SendByte(Socket socket, byte data, byte ID){
+   
+        }
     }
 }
